a83d08c8e3f5e93bdceb46156917ed6c54e63106
Kevin Zuniga <kevin.zuniga@pucp.pe>
2019-05-09 19:36:06 -0500

    Updates

---
+++ precache-manifest.feceff37d8a3bd130262e115679a9aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eacc6ae785be09c0140",
+    "url": "/customer/static/css/main.ee1607a4.chunk.css"
+  },
+  {
+    "revision": "0eacc6ae785be09c0140",
+    "url": "/customer/static/js/main.5b673b08.chunk.js"
+  },
+  {
+    "revision": "e69f442ad8c78409ee3a",
+    "url": "/customer/static/js/runtime~main.c790c1bc.js"
+  },
+  {
+    "revision": "9a165016fe396fa13979",
+    "url": "/customer/static/css/2.48bcfd89.chunk.css"
+  },
+  {
+    "revision": "9a165016fe396fa13979",
+    "url": "/customer/static/js/2.20e8f075.chunk.js"
+  },
+  {
+    "revision": "1d6da6acd851e6381ec00d4e6db3162c",
+    "url": "/customer/index.html"
+  }
+];
